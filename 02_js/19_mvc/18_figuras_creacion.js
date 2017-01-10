db93e64897e646b2c7d107c4b0ec0758596cb99d
G03-EQ15\tarde <jdaveruiz@gmail.com>
2017-01-10 20:59:34 +0100

    Día 10-01-2017

--- 02_js/19_mvc/18_figuras_creacion.js
+++ 02_js/19_mvc/18_figuras_creacion.js
@@ -1,11 +1,10 @@
 var padreCalcRectangulo = document.getElementById("padre-Calc-Rectangulo");
 var padreCalcTriangulo = document.getElementById("padre-Calc-Triangulo");
 var padreCalcElipse = document.getElementById("padre-Calc-Elipse");
+var padreCalcHuevo = document.getElementById("padre-Calc-Huevo");
 
-var vistaRectangulo = new FiguraVistaV2(padreCalcRectangulo, "Rectangulo");
-var vistaTriangulo = new FiguraVistaV2(padreCalcTriangulo, "Triangulo");
-var vistaElipse = new FiguraVistaV2(padreCalcElipse, "Elipse");
+var vistaRectangulo = new FiguraVista(padreCalcRectangulo, "Rectangulo");
+var vistaRectangulo2 = new FiguraVistaV2(padreCalcRectangulo, "Rectangulo");
 
 var controlRectangulo = new FiguraControlador(vistaRectangulo, Modelo.Rectangulo);
-var controlTriangulo = new FiguraControlador(vistaTriangulo, Modelo.TrianguloEquilatero);
-var controlElipse = new FiguraControlador(vistaElipse, Modelo.Elipse);
+var controlRectangulo2 = new FiguraControlador(vistaRectangulo2, Modelo.Rectangulo);
