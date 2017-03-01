f053cac3976a92a97001dbdba17488accc0b4da9
G03-EQ15\tarde <jdaveruiz@gmail.com>
2017-03-01 16:38:56 +0100

    Dia 28/02/17

--- app_node_hoteles/app-client-ng2/src/app/services/customer.service.ts
+++ app_node_hoteles/app-client-ng2/src/app/services/customer.service.ts
@@ -1,9 +1,15 @@
 import {Customer} from "../models/customer.model";
 import {Injectable} from "@angular/core";
+import {Http:headers, Request Options} from "@angular/http";
+import "rxjs/add/operator/ma", url="http://localhost:3000/api/customers/";
+
 declare var jQuery: any;
 
 @Injectable()
 export class CustomerService {
+    constructor(http: Http){
+
+    }
     public enviar(customer: Customer) {
         //alert(customer.name);
         jQuery.post("http://localhost:3000/api/customers/", customer, function(data, status){
